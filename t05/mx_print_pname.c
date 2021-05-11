b974d54dbcb8da46da2587ac25d7d056e7ba2bff
yskorokhod <yskorokhod@student.ucode.world>
2021-05-11 14:21:33 +0300

    5

---
+++ t05/mx_print_pname.c
@@ -0,0 +1,9 @@
+char *mx_strchr(const char *s, int c);
+void mx_printchar(char c);
+void mx_printstr(const char *s);
+int main(int argc, char *argv[])
+{
+  char *str = mx_strchr(argv[argc - 1], '/');
+  mx_printstr(++str);
+  mx_printchar('\n');
+}
