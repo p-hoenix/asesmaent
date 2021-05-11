303be3e7b41e278aa67e4e2205bccc51d93d97d6
yskorokhod <yskorokhod@student.ucode.world>
2021-05-11 14:49:39 +0300

    7

---
+++ t06/mx_isspace.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+bool mx_isspace(char c)
+{
+  if(c == ' ')
+    {
+      return true;
+    }
+  else
+    {
+      return false;
+    }
+}
---
+++ t07/mx_pow_rec.c
@@ -0,0 +1,11 @@
+double mx_pow_rec(double n, unsigned int pow)
+{
+  if (pow)
+    {
+      return (n * mx_pow_rec(n, pow - 1));
+    }
+  else
+    {
+      return 1;
+    }
+}
