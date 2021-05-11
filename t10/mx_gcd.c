3ed87b0a3f59b6f2b6155950290e30f762fdbafd
yskorokhod <yskorokhod@student.ucode.world>
2021-05-11 15:15:14 +0300

    10

---
+++ t10/mx_gcd.c
@@ -0,0 +1,15 @@
+int mx_gcd(int a, int b)
+{
+  if (a  < 0)
+    {
+      a *= -1;
+    }
+  if(b < 0)
+    {
+      b *= -1;
+    }
+  if (b != 0)
+    return mx_gcd(b, a % b);
+  else
+    return a;
+}
