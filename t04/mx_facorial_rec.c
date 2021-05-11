025fb0cf3537c6ca73925ee56df15e4abe1cabf1
yskorokhod <yskorokhod@student.ucode.world>
2021-05-11 14:11:53 +0300

    4

---
+++ t04/mx_facorial_rec.c
@@ -0,0 +1,14 @@
+#include <limits.h>
+int mx_factorial_rec(int n)
+{
+  if(n > INT_MAX)
+    {
+      return 0;
+    }
+  if (n >= 1)
+    {
+      return n * mx_factorial_rec(n - 1);
+    }
+  else
+    return 1;
+}
