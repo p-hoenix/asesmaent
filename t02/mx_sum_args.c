a8dd68db65b8ec1c3c195725687e107fbdfeb8a7
yskorokhod <yskorokhod@student.ucode.world>
2021-05-14 15:09:11 +0300

    3

--- t02/mx_sum_args.c
+++ t02/mx_sum_args.c
@@ -4,12 +4,13 @@ int mx_atoi(const char *str);
 bool mx_isspace(char c);
 void mx_printchar(char c);
 void mx_printint(int n);
+
 int main(int argc, char *argv[])
 {
   int sum = 0;
-  for (int i = 1; i <= argc - 1; i++)
-    {
-      sum += mx_atoi(argv[i]);
-    }
+  for (int i = 1; i <= argc - 1; i++);
   mx_printint(sum);
+  mx_printchar('\n');
+  
+  return 0;
 }
